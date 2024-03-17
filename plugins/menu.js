70e5e5f47da0056ae1f25a1c561d2c9a98fd4df7
zizozZ <79533540+zizozZ@users.noreply.github.com>
2024-03-18 01:45:57 +0200

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -156,7 +156,7 @@ renderLargerThumbnail: true
     throw e
   }
 }
-handler.help = ['menu']
+handler.help = ['اوامر']
 handler.tags = ['infobot']
 handler.command = ['menu','b','list'] 
 handler.register = false
